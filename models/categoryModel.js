d15b2e1d758b5f3f079bb5ea19c8bfc6d7c73882
esraa <esraaalrassas@gmail.com>
2025-10-22 18:00:23 +0300

    wwwwwwwwwwwww

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -23,16 +23,21 @@ const categorySchema = new mongoose.Schema(
 
 
 
-const setImageURL = (doc) => {
-  if (doc.image && !doc.image.startsWith('http')) {
-    doc.image = `${process.env.BASE_URL}/categories/${doc.image}`;
+ const setImageURL = (doc) => {
+  if (doc.image) {
+    const imageUrl = `${process.env.BASE_URL}/categories/${doc.image}`;
+    doc.image = imageUrl;
   }
 };
 
-categorySchema.post('init', (doc) => setImageURL(doc));
-categorySchema.post('save', (doc) => setImageURL(doc));
+categorySchema.post('init', (doc) => {
+  setImageURL(doc);
+});
 
-
+// create
+categorySchema.post('save', (doc) => {
+  setImageURL(doc);
+});
 
   categorySchema.pre("deleteMany", async function (next) {
     const categories = await this.model.find(this.getFilter()); // جلب جميع التصنيفات قبل الحذف
